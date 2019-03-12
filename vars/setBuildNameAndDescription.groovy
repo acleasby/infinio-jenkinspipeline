e41230c91b5219ef88555c2a8c186fd1b39a3836
Andrew Cleasby <acleasby@infinio.com>
2019-03-12 11:03:18 -0400

    Additional description

--- vars/setBuildNameAndDescription.groovy
+++ vars/setBuildNameAndDescription.groovy
@@ -2,7 +2,7 @@
  * Sets BUILD_IDENT if not already set, also sets Jenkins build name and description.
  * @return
  */
-def call() {
+def call(def additionalDescription) {
     String buildIdent = env['BUILD_IDENT'] ?: ""
     String gitBranch = env['GIT_BRANCH'] ?: "master"
     String testSuite = env['testSuite'] ?: null
@@ -17,6 +17,7 @@ def call() {
 
     String buildName = "#" + currentBuild.number + " " + "$buildIdent-$gitBranch"
     String buildDescription = "build #: $buildNumber, branch: ${gitBranch}" + (testSuite != null ? ", suite: $testSuite" : "")
+    buildDescription = buildDescription + (additionalDescription ?: "")
 
     // Set Jenkins build name and description
     currentBuild.displayName = buildName
